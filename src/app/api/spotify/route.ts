4c7763a22162b7591f386f3765a2529058b692ca
skidoodle <contact@albert.lol>
2023-05-20 19:43:53 +0200

    xxx

--- src/app/api/spotify/route.ts
+++ src/app/api/spotify/route.ts
@@ -7,18 +7,8 @@ const spotify = new SpotifyService(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN)
  
 export async function GET() {
     const song: any = await spotify.getCurrentTrack()
-
-    if(!song.isPlaying) {
-        return NextResponse.json({ nowplaying: false })
-    }
     
     return NextResponse.json({
-        nowplaying: true,
-        track: {
-            artist: song.artists.name,
-            title: song.title,
-            url: song.url,
-            image: song.album.image
-        }
+        song
     })
 }
